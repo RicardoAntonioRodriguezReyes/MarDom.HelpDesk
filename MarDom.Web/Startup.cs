d1a1c539711e74cf406ebd54d99b96f426d77346
ricardoantoniorodriguezreyes <ricardoantoniorodriguezreyes@hotmail.com>
2017-05-24 22:26:18 -0400

    ok

---
+++ MarDom.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarDom.Web.Startup))]
+namespace MarDom.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
