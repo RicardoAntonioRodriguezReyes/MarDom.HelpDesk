74c14d61aacfde1750540cee7c465bd4471b61b8
ricardoantoniorodriguezreyes <ricardoantoniorodriguezreyes@hotmail.com>
2017-05-25 23:17:42 -0400

    Configuracion de template y pantallas principales

---
+++ MarDom.Web/Controllers/TicketController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarDom.Web.Controllers
+{
+    public class TicketController : Controller
+    {
+        [Authorize]
+        // GET: Ticket
+
+
+       public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Solicitud()
+        {
+            return View();
+        }
+    }
+}
